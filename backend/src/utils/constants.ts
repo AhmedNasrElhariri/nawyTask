44965be1880471ce0addee31e75c240627531df2
Ahmed Nasr <ahmednasr.fci97@gmail.com>
2025-10-04 12:27:18 +0300

    add docker

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,43 +1,6 @@
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
-
-export const PAGE_SIZE = 10;
-
-export const PAYMENT_STATUS = {
-  PENDING: "PENDING",
-  COMPLETED: "COMPLETED",
-  FAILED: "FAILED",
-  CANCELED: "CANCELED",
-};
-
-export const SUBSCRIPTIONS_ITEM_TYPES = {
-  COURSES: "COURSES",
-  INDICATORS: "INDICATORS",
-  BOOKS: "BOOKS",
-  NEWS_CATEGORY: "NEWS_CATEGORY",
-  SIGNALS_CATEGORY: "SIGNALS_CATEGORY",
-};
-
-export const ADMINS_TYPES = {
-  ADMIN: "Admin",
-  SUPERADMIN: "SuperAdmin",
-  MEMBER: "Member",
-};
-
-export const MODEL_TYPES = {
-  REGISTRATION: "Registration",
-  SUBSCRIPTION: "Subscription",
-  COURSE: "Course",
-};
-
 export const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:3000",
   "http://localhost:3001",
   "https://motaalem-website.vercel.app",
 ];
-
-export const ACCESS_TYPES = {
-  SUBSCRIPTION: "subscription",
-  COURSE: "course",
-  LESSON: "lesson",
-};
